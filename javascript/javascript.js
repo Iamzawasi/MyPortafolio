ef4a763f7daac128cc3c1d9e56ad3196d7a8b97d
Ahmad Zia Wasi <zia.ah2015@gmail.com>
2022-03-20 21:17:54 +0100

    New Year popup added

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -13,3 +13,25 @@ function closeMenu() {
   document.querySelector("#navigation__background").style.zIndex = "0";
   document.querySelector("#navigation__background").style.visibility = "hidden";
 }
+$message = setTimeout(mymessage, 5000);
+function mymessage() {
+  sty = document.getElementById("PopupNew").style;
+  sty.display = "none";
+}
+
+// IntervalForTimeOutVar = setInterval(CheckTimeOutVar, 31000);
+// function CheckTimeOutVar() {
+//   TimeOutVar ? (TimeOutVar = false) : 0; ///Zia: if TimeOutVar is true then change its value to false
+//   console.log(TimeOutVar);
+// }
+// addEventListener("click", updateTimeOut); /// Zia: if Someone Click on any part of the Page it will Update TimeOutVar from false to True
+// function updateTimeOut() {
+//   TimeOutVar = true;
+//   console.log(TimeOutVar);
+//   clearInterval(IntervalForTimeOutVar);
+//   IntervalForTimeOutVar = setInterval(CheckTimeOutVar, 31000);
+// }
+// var x = parseInt(parsdocument.getElementById("nothing"));
+// if (typeof (x != "undefined")) {
+//   console.log("its working" + document.getElementById("nothing").length);
+// }
