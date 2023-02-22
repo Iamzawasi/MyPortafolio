1c2beab6eb29707ff067d545194d0427ce6c4a6a
Ahmad Zia Wasi <zia.ah2015@gmail.com>
2023-02-22 23:50:03 +0100

    New year message added for persion new year

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -13,30 +13,38 @@ function closeMenu() {
   document.querySelector("#navigation__background").style.zIndex = "0";
   document.querySelector("#navigation__background").style.visibility = "hidden";
 }
-//$message = setTimeout(mymessage, 5000);
+let today = new Date().toISOString().slice(0, 10);
 sty = document.getElementById("PopupNew").style;
-sty.display = "none";
-// function mymessage() {
-//   sty = document.getElementById("PopupNew").style;
-//   sty.display = "none";
-// }
+if (
+  today.toString() == "2023-03-20" ||
+  today.toString() == "2023-03-21" ||
+  today.toString() == "2023-03-22"
+) {
+  console.log(today);
+  $message = setTimeout(mymessage, 5000);
 
-// IntervalForTimeOutVar = setInterval(CheckTimeOutVar, 31000);
-// function CheckTimeOutVar() {
-//   TimeOutVar ? (TimeOutVar = false) : 0; ///Zia: if TimeOutVar is true then change its value to false
-//   console.log(TimeOutVar);
-// }
-// addEventListener("click", updateTimeOut); /// Zia: if Someone Click on any part of the Page it will Update TimeOutVar from false to True
-// function updateTimeOut() {
-//   TimeOutVar = true;
-//   console.log(TimeOutVar);
-//   clearInterval(IntervalForTimeOutVar);
-//   IntervalForTimeOutVar = setInterval(CheckTimeOutVar, 31000);
-// }
-// var x = parseInt(parsdocument.getElementById("nothing"));
-// if (typeof (x != "undefined")) {
-//   console.log("its working" + document.getElementById("nothing").length);
-// }
+  function mymessage() {
+    sty = document.getElementById("PopupNew").style;
+    sty.display = "none";
+  }
+
+  IntervalForTimeOutVar = setInterval(CheckTimeOutVar, 31000);
+  function CheckTimeOutVar() {
+    TimeOutVar ? (TimeOutVar = false) : 0; ///Zia: if TimeOutVar is true then change its value to false
+    console.log(TimeOutVar);
+  }
+  addEventListener("click", updateTimeOut); /// Zia: if Someone Click on any part of the Page it will Update TimeOutVar from false to True
+  function updateTimeOut() {
+    TimeOutVar = true;
+    console.log(TimeOutVar);
+    clearInterval(IntervalForTimeOutVar);
+    IntervalForTimeOutVar = setInterval(CheckTimeOutVar, 31000);
+    sty.display = "none";
+  }
+} else {
+  console.log("not today");
+  sty.display = "none";
+}
 function bgcolChange() {
   inpRight = document.getElementById("color-right").value;
   inpLeft = document.getElementById("color-left").value;
